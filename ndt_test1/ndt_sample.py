9a5263bd3270aea581aeb62f5538040dad99ade9
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2022-01-17 19:38:14 +0900

    Add test case.

--- ndt_test1/ndt_sample.py
+++ ndt_test1/ndt_sample.py
@@ -1,48 +1,25 @@
 import os
 import sys
-import copy
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.spatial.transform import Rotation
 osp = os.path
 np.random.seed(0)
 sys.path.append(osp.join(osp.dirname(osp.abspath(__file__)), "build"))
+
 import libndt_sample
 
-def visualize_pc(ax, pc, label):
-    ax.scatter3D(pc[:, 0], pc[:, 1], pc[:, 2], label=label)
-
-mu = 0
-sigma = 1.0
-
 # create reference point cloud
-sample_count = 100
-reference_pc = np.random.normal(mu, sigma, (sample_count, 3))
+point = np.array([0, 0 ,0], dtype=np.float32)
+transform = np.array([0, 0, 0, 30, 0, 0], dtype=np.float32) # tx, ty, tz, rx, ry, rz
 
 # create map
 ndt = libndt_sample.NDT()
-ndt.create_map(reference_pc)
+score, jacobian, hessian = ndt.calcScore(point, transform)
 
-# create scan point cloud
-euler = np.array([ 5.0, 0, 0.0])
-rot = Rotation.from_euler('zyx', euler, degrees=True)
-trans = np.array([ 0.1, 0.0, 0.0])
-scan_pc = np.apply_along_axis(lambda x: rot.apply(x) + trans, 1, reference_pc)
+jacobian = list(map(float, jacobian))
 
-# registration and get result transform.
-# TODO: use boost python
+jacobian = np.array(list(map(float, jacobian)), dtype=np.float32).reshape((6, 3)).T
+hessian = np.array(list(map(float, hessian)), dtype=np.float32).reshape((6, 18)).T
 
-transform = np.eye(4)
-ans_pc = np.apply_along_axis(lambda x: transform[:3, :3].dot(x) + transform[0:3, 3], 1, scan_pc)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-visualize_pc(ax, reference_pc, "reference")
-visualize_pc(ax, scan_pc, "scan")
-visualize_pc(ax, ans_pc, "registered")
-
-ax.legend()
-
-plt.show()
+print(score)
+print(jacobian)
+print(hessian)
