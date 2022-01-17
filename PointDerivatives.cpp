9e70b6194bbe1f655281b6d7cb62a2380802cc7a
7oei <adachi-n@roboken.iit.tsukuba.ac.jp>
2022-01-17 19:35:31 +0900

    fix for readability

--- PointDerivatives.cpp
+++ PointDerivatives.cpp
@@ -11,8 +11,11 @@ int main(){
     PointT mean;
     mean << 0,0,0;
     CovarianceMatrixT cov = MatrixXd::Identity(3, 3);
-    CovarianceMatrixT cov_inv = MatrixXd::Identity(3, 3);
-    cov_inv = cov.inverse();
+    CovarianceMatrixT cov_inv = cov.inverse();
+    double output_prob = 0.055;
+    double resolution = 1.0;
+    PointT x_k_dash = point - mean;
+    
     // cout << tf << endl;
     // cout << endl;
     // cout << cov << endl;
@@ -21,6 +24,7 @@ int main(){
     JacobianCoefficientsT jacobian_coefficients;
     HessianCoefficientsT hessian_coefficients;
     angleDerivatives(tf,jacobian_coefficients,hessian_coefficients);
+
     cout << "Jacobian Coefficients is " << endl;
     cout << jacobian_coefficients << endl;
     cout << endl;
@@ -31,6 +35,7 @@ int main(){
     PointJacobianT point_jacobian;
     PointHessianT point_hessian;
     pointDerivatives(point,jacobian_coefficients,hessian_coefficients,point_jacobian,point_hessian);
+
     cout << "Point Jacobian is " << endl;
     cout << point_jacobian << endl;
     cout << endl;
@@ -38,15 +43,12 @@ int main(){
     cout << point_hessian << endl;
     cout << endl;
 
-    double output_prob = 0.055;
-    double resolution = 1.0;
     ScoreParams params(output_prob, resolution);
+
     cout << "d Params is " << params.d(1)<< "," << params.d(2) << "," << params.d(3) << endl;
 
-    PointT x_k_dash;
-    x_k_dash = point - mean;
-
     tuple<double, JacobianT, HessianT> derivatives = computeDerivative(params, point_jacobian, point_hessian, x_k_dash, cov_inv);
+
     cout << "Score is "<< get<0>(derivatives) << endl;
     cout << endl;
     cout << "Jacobian is " << endl;
